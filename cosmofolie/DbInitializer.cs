ab581fe7f06dae8d55f1a027849a655620a02ead
EVN <EVN@SWCSLEVA>
2023-06-21 17:30:50 +0200

    Totalement fini

--- cosmofolie/DbInitializer.cs
+++ cosmofolie/DbInitializer.cs
@@ -21,6 +21,7 @@ public static class DbInitializer
                 " qui orbite autour du Soleil, à une distance d'environ 1,5 million de km de la Terre.\r\n\r\nLes images renvoyées étaient issues d'un" +
                 " essai visant à tester l'alignement des instruments de ce fabuleux engin qui a couté la faramineuse somme de 10 milliards de dollars à développer." +
                 "Découvrons donc ces premières images de ce télescope 100 fois plus puissants que l'ancien télescope spatial Hubble!",
+                
               
             }); 
         roleManager.CreateAsync(new IdentityRole(Constants.AdminRole)).GetAwaiter().GetResult();
